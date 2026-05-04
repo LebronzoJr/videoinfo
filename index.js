300e01c1eab4d151a56f78d65cd7af7bcc3a8018
Lebron <alebron1290@gmail.com>
2026-05-04 10:44:36 -0400

    AddedPackages

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const { app, BrowserWindow } = electron;
 
 app.on('ready', ()=>{
     console.log ("App is ready");
-    const mainWindow = new BrowserWindow({});
+    const mainWindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
