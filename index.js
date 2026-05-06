1211ee54222544eb745219bb91ffab1ff8149126
Lebron <alebron1290@gmail.com>
2026-05-06 10:11:17 -0400

    FixinScripts

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const ffmpeg = require('fluent-ffmpeg');
 
+const { app, BrowserWindow } = electron;
+const remote = require ('@electron/remote/main');
+remote.initialize();
+// const app = electron.app;
 
 app.on('ready', ()=>{
-    console.log ("App is ready");
+    console.log("App is ready");
     const mainWindow = new BrowserWindow({
+        height:800,
+        width:600,
         webPreferences:{
             nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: false,
+            enableRemoteModule:true,
         }
     });
+    remote.enable(mainWindow.webContents);
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
+//npm install electron
+//npm install @electron/remote
+//npm install fluent-ffmpeg
